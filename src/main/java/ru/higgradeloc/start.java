15ed8f5cf243a2a31f899ea63e82a12682066f87
hedggehogger-rgb <hedggehogger@gmail.com>
2025-12-29 09:10:14 +0300

    Update start.java

--- src/main/java/ru/higgradeloc/start.java
+++ src/main/java/ru/higgradeloc/start.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 public class start {
     public static void main(String[] args) {
-        // --- 1. Инициализация персонажей и среды ---
         System.out.println("=== НАЧАЛО СЦЕНЫ: Выселение Кума Тыквы ===");
 
         pumpkin pump = new pumpkin("Кум Тыква");
@@ -18,47 +17,30 @@ public class start {
         List<villager> neighbors = new ArrayList<>();
         neighbors.add(new villager("Сосед Лук"));
         neighbors.add(new villager("Тетушка Фасоль"));
-
-        // --- 2. Сценарий ---
-
-        System.out.println("\n--- НАТ. ДОМИК КУМА ТЫКВЫ ---");
-
-        // Кум Тыква пытается уйти от реальности
+        System.out.println("\n--- ДОМИК КУМА ТЫКВЫ ---");
         pump.performAction();
         pump.dream();
-
-        // Помидор начинает действовать
         tomato.performAction();
         tomato.grab(pumpkinHouse);
 
         try {
-            // Помидор трясет домик
             tomato.shake(pumpkinHouse);
         } catch (StructuralDamageException e) {
-            // Обработка Checked исключения
             System.err.println("[EXCEPTION LOG]: " + e.getMessage());
             System.out.println(">> Из-за разрушений Кум Тыква вынужден вернуться в реальность.");
             pump.openEyes();
         } catch (IllegalArgumentException e) {
             System.out.println("Ошибка аргумента: " + e.getMessage());
         }
-
-        // Рычание Помидора
         tomato.growl();
-
-        // Реакция жителей
-        System.out.println("\n--- НАТ. УЛИЦА ДЕРЕВНИ ---");
+        System.out.println("\n--- УЛИЦА ДЕРЕВНИ ---");
 
         for (villager villager : neighbors) {
             villager.performAction();
         }
-
-        // Появление Зеленого Горошка
         System.out.println("\n--- ВНЕЗАПНО ---");
 
         greenpea pea = new greenpea("Синьор Зеленый Горошек");
-
-        // Анонимный класс
         Runnable appearAction = new Runnable() {
             @Override
             public void run() {
@@ -70,10 +52,6 @@ public class start {
         pea.performAction();
 
         System.out.println("\n=== КОНЕЦ СЦЕНЫ ===");
-
-        // Технический вывод (toString)
         System.out.println("\n[DEBUG INFO]:");
-        System.out.println(pump.toString());
-        System.out.println(pumpkinHouse.toString());
     }
 }
